20f5800f9dbe85af90e26681acf5f480f32c6efd
Una L'Estrange <lestange.una@itsligo.ie>
2017-02-12 15:16:04 +0000

    Added some more projects for questions from the worksheet.

---
+++ WorkSheet1Revision/Part3Q1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            A year with 366 days is called a leap year.
+            A is a leap year if it is divisible by four, 
+                    except that it is not a leap year 
+                if it is divisible by 100; however it is a leap year 
+                if it is divisible by 400.There were no exceptions 
+                before the introduction of the Gregorian year in 1582.
+                Write a program that asks the user for a year and computes 
+                whether that year is a leap year.
+                */
+
+
+      Console.Write("Please enter a year or -999 to quit > ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+
+            // the question didn't look for a loop but it made it easier to test.
+
+            while (year != -999)
+            {
+
+                if (year % 400 == 0)
+                {
+                    Console.WriteLine("That is a leap year");
+                }
+                else if (year < 1582 && (year % 4 == 0))
+                {
+                    Console.WriteLine("That is a leap year");
+                }
+                else if (year % 4 == 0 && year % 100 != 0)
+                {
+                    Console.WriteLine("That is a leap year");
+                }
+                else
+                {
+                    Console.WriteLine("That is not a leap year");
+                }
+
+                Console.Write("Please enter a year or -999 to quit > ");
+                year = Convert.ToInt32(Console.ReadLine());
+            } // end of the while
+        }
+    }
+}
+
---
+++ WorkSheet1Revision/Part3Q2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3Q2
+{
+    class Program
+    {
+        /*Write a program to print the pattern. 
+        Printing one ‘#’ at a time
+        #
+        # #
+        # # #
+        # # # # 
+        # # # # #
+        Do it now upside down
+        */
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+
+            /* upside down */
+
+            for (int i = 5; i > 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
